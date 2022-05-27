7de8a0a1882df122599c24f7faef0d3fd56a303b
sneeu-leeu <sa070197@gmail.com>
2022-05-27 13:03:28 +0200

    Add: update devise rules in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,15 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :success, :error, :warning, :message
+  protect_from_forgery prepend: true
+
+
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name])
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
   end
 end
