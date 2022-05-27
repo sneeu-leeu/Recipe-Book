b140a1ae696cac733e9e57c6ef29eb570dbfa391
sneeu-leeu <sa070197@gmail.com>
2022-05-27 13:00:01 +0200

    Add: model associations and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # validates :name, presence: true, length: { minimum: 3 }
+  has_many :foods, dependent: :destroy
+  has_many :recipes, dependent: :destroy
+  has_many :inventories, dependent: :destroy
+
+  # def set_role
+  #   update(role: 'user')
+  # end
 end
