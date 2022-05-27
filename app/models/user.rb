9e671a40918388607ee951a2966094cc92388a13
sneeu-leeu <sa070197@gmail.com>
2022-05-27 15:01:26 +0200

    Feature: update cancan auth rules in views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,22 @@
 class User < ApplicationRecord
+  after_create :set_role
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  # validates :name, presence: true, length: { minimum: 3 }
+
+  validates :name, presence: true, length: { minimum: 3 }
   has_many :foods, dependent: :destroy
   has_many :recipes, dependent: :destroy
   has_many :inventories, dependent: :destroy
 
   def set_role
     update(role: 'user')
+  end
+
+  def admin?
+    false
   end
 
   def sign_out
